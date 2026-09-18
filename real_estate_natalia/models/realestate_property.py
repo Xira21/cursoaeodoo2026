d9bceb8589a8af81b062eff425207ebdc31afe76
Natalia <natalialaguna9681@gmail.com>
2026-09-18 15:16:44 +0000

    Tutoria2: Se añaden los modelos categoria, contrato y oferta

--- real_estate_natalia/models/realestate_property.py
+++ real_estate_natalia/models/realestate_property.py
@@ -8,11 +8,15 @@ class RealEstateProperty(models.Model):
     description = fields.Text(string="Description")
     price = fields.Float(string="Price")
     reference = fields.Char(string="Reference")
-    availability = fields.Boolean(string="Availability", default="True")
+    availability = fields.Boolean(string="Availability", default=True)
     user_id = fields.Many2one(
         comodel_name="res.users",
         string="User",
     )
-
+    category_id = fields.Many2one(
+        comodel_name="realestate.category",
+        string="Category",
+    )
+    
     def action_reserve(self):
         self.availability = False
